8ef60a1875b00b7e7ecca17eb167326d930bbd3e
Tim147 <tim147@outlook.com>
2015-03-12 20:23:04 +0800

    your comment

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <sys/time.h>
+#include <cstdio>
+#include <unistd.h>
+
+using namespace std;
+
+int main(){
+    float exe_time = 0;
+    struct timeval start;
+    struct timeval end;
+    gettimeofday(&start,NULL);
+    printf("start.tv_sec: %d\n",start.tv_sec);
+    printf("start.tv_usec: %d\n",start.tv_usec);
+
+    sleep(10);
+
+    gettimeofday(&end,NULL);
+    printf("end.tv_sec: %d\n", end.tv_sec);
+    printf("end.tv_usec: %d\n",end.tv_usec);
+    exe_time = (end.tv_sec - start.tv_sec)*1000000+(end.tv_usec-start.tv_usec);
+    printf("time used = %f\sn",exe_time/1000000);
+    return 0;
+}
